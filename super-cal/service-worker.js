f726be0507da84573f67f1c5fc150422d6fde787
Chalksfree <124820611+Chalksfree@users.noreply.github.com>
2026-09-05 08:23:37 -0400

    Use Windows side space for daily item panels

--- super-cal/service-worker.js
+++ super-cal/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='super-cal-static-v20260906';
-const ASSETS=['./','./index.html','./styles.css?v=20260906','./core.js?v=20260906','./app.js?v=20260906','./manifest.webmanifest?v=20260906','./icon.svg?v=20260906'];
+const CACHE='super-cal-static-v20260907';
+const ASSETS=['./','./index.html','./styles.css?v=20260907','./core.js?v=20260907','./app.js?v=20260907','./manifest.webmanifest?v=20260907','./icon.svg?v=20260907'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request).then(response=>{if(new URL(event.request.url).origin===location.origin){const copy=response.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));}return response}).catch(()=>caches.match('./index.html'))));});
